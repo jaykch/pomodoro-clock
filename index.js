e613bec02bc4be6464a1b2a7c3dbc88717170b02
Jay Kumar <jaykch@outlook.com>
2016-11-14 16:02:24 +0000

    Designed view

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 /**
  * Created by Jay on 11/14/2016.
  */
+
+$(document).ready(function(){
+
+
+});
+
+var breakTime = "1:00", sessionTime = "25:00", displayTime = "25:00";
+
+var pomodoro = {
+
+    init: function(){
+
+    },
+    reset
+};
