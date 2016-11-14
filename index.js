e15da439511df684447e2501e97f2403d42fe4d4
Jay Kumar <jaykch@outlook.com>
2016-11-14 17:26:13 +0000

    Finished button functionality

--- index.js
+++ index.js
@@ -2,17 +2,93 @@
  * Created by Jay on 11/14/2016.
  */
 
-$(document).ready(function(){
-
+$(document).ready(function () {
+    pomodoro.init();
 
 });
 
-var breakTime = "1:00", sessionTime = "25:00", displayTime = "25:00";
 
 var pomodoro = {
 
-    init: function(){
+
+    config: {
+        breakVal: 5,
+        sessionVal: 25,
+        displayMinVal: 25,
+        displaySecVal: 0
+
 
     },
-    reset
+
+    init: function () {
+        this.getDomElements();
+        this.reset();
+        this.resetHandler();
+        this.render();
+        this.sessionSelectHandler();
+        this.breakSelectHandler();
+    },
+
+    getDomElements: function () {
+        this.$break = $('#break-display');
+        this.$session = $('#session-display');
+        this.$display = $('#display');
+        this.$reset = $('#reset');
+        this.$sneg = $('#session-neg');
+        this.$spos = $('#session-pos');
+        this.$bneg = $('#break-neg');
+        this.$bpos = $('#break-pos');
+    },
+
+    resetHandler: function () {
+        this.$reset.click(function () {
+            this.reset();
+            console.log("resetting....");
+        }.bind(this));
+    },
+
+    sessionSelectHandler: function () {
+        this.$sneg.click(function () {
+            if (this.config.sessionVal > 1)this.config.sessionVal -= 1;
+            this.config.displayMinVal = this.config.sessionVal;
+            this.render();
+        }.bind(this));
+        this.$spos.click(function () {
+            this.config.sessionVal += 1;
+            this.config.displayMinVal = this.config.sessionVal;
+            this.render();
+        }.bind(this));
+    },
+    breakSelectHandler: function () {
+        this.$bneg.click(function () {
+            if (this.config.breakVal > 1)this.config.breakVal -= 1;
+            this.render();
+        }.bind(this));
+        this.$bpos.click(function () {
+            this.config.breakVal += 1;
+            this.render();
+        }.bind(this));
+    },
+
+    resetVals: function () {
+        this.config.breakVal = 5;
+        this.config.sessionVal = 25;
+        this.config.displayMinVal = 25;
+    },
+
+    reset: function () {
+        this.resetVals();
+        this.render();
+    },
+
+    render: function () {
+        this.$break.html(this.config.breakVal + ":00");
+        this.$session.html(this.config.sessionVal + ":00");
+        if (this.config.displaySecVal < 10) {
+            this.$display.html(this.config.displayMinVal + ":0" + this.config.displaySecVal);
+        }
+        else {
+            this.$display.html(this.config.displayMinVal + this.config.displaySecVal);
+        }
+    }
 };
